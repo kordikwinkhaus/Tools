8ebc3c1a1c8bba38954bf2fa4e052604e3470615
Tomas Kordik <kordik@winkhaus.cz>
2015-05-22 15:49:46 +0200

    Český překlad hlavního programu

--- Tools/MainWindow.Designer.cs
+++ Tools/MainWindow.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             this.label1 = new System.Windows.Forms.Label();
             this.lstTools = new System.Windows.Forms.ListBox();
             this.cmdRun = new System.Windows.Forms.Button();
@@ -36,55 +37,37 @@
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Available tools:";
             // 
             // lstTools
             // 
-            this.lstTools.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.lstTools, "lstTools");
             this.lstTools.DisplayMember = "Name";
             this.lstTools.FormattingEnabled = true;
-            this.lstTools.Location = new System.Drawing.Point(12, 25);
             this.lstTools.Name = "lstTools";
-            this.lstTools.Size = new System.Drawing.Size(234, 212);
-            this.lstTools.TabIndex = 1;
             this.lstTools.DoubleClick += new System.EventHandler(this.lstTools_DoubleClick);
             // 
             // cmdRun
             // 
-            this.cmdRun.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.cmdRun.Location = new System.Drawing.Point(252, 25);
+            resources.ApplyResources(this.cmdRun, "cmdRun");
             this.cmdRun.Name = "cmdRun";
-            this.cmdRun.Size = new System.Drawing.Size(75, 23);
-            this.cmdRun.TabIndex = 2;
-            this.cmdRun.Text = "Run";
             this.cmdRun.UseVisualStyleBackColor = true;
             this.cmdRun.Click += new System.EventHandler(this.cmdRun_Click);
             // 
             // cmdClose
             // 
-            this.cmdClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.cmdClose, "cmdClose");
             this.cmdClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cmdClose.Location = new System.Drawing.Point(252, 54);
             this.cmdClose.Name = "cmdClose";
-            this.cmdClose.Size = new System.Drawing.Size(75, 23);
-            this.cmdClose.TabIndex = 3;
-            this.cmdClose.Text = "Close";
             this.cmdClose.UseVisualStyleBackColor = true;
             this.cmdClose.Click += new System.EventHandler(this.cmdClose_Click);
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cmdClose;
-            this.ClientSize = new System.Drawing.Size(335, 255);
             this.Controls.Add(this.cmdClose);
             this.Controls.Add(this.cmdRun);
             this.Controls.Add(this.lstTools);
@@ -93,8 +76,6 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MainWindow";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Tools";
             this.ResumeLayout(false);
             this.PerformLayout();
 
