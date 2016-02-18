535fc1d497a37e54d5bd33e14af26b1865696a2e
Tomas Kordik <kordik@winkhaus.cz>
2016-02-18 17:13:53 +0100

    Oficiální kompilace

--- OfferOptTexts/Properties/AssemblyInfo.cs
+++ OfferOptTexts/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Winkhaus CR, s.r.o.")]
 [assembly: AssemblyProduct("OfferOptTexts")]
-[assembly: AssemblyCopyright("Copyright © Winkhaus CR, s.r.o. 2015")]
+[assembly: AssemblyCopyright("Copyright © Winkhaus CR, s.r.o. 2015-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
