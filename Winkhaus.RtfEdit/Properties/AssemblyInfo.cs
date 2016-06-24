f9910cba979ed65bc89eccb8d32c11be9a166548
Tomas Kordik <kordik@winkhaus.cz>
2016-06-24 11:05:36 +0200

    Oficiální kompilace

--- Winkhaus.RtfEdit/Properties/AssemblyInfo.cs
+++ Winkhaus.RtfEdit/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("Winkhaus.RtfEdit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Winkhaus CR, s.r.o.")]
 [assembly: AssemblyProduct("Winkhaus.RtfEdit")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Winkhaus CR, s.r.o. 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.4.1.0")]
+[assembly: AssemblyFileVersion("1.4.1.0")]
